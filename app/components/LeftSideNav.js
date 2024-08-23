0fb4722daaca5f895524b0ea85a68bcd306aac62
Your Name <you@example.com>
2024-08-23 16:35:43 +0530

    change

--- app/components/LeftSideNav.js
+++ app/components/LeftSideNav.js
@@ -21,7 +21,7 @@ function LeftSideNav() {
   return !authUser ? (
     <Loading />
   ) : (
-    <div className="md:flex min-h-screen hidden">
+    <div className="lg:flex min-h-screen hidden">
       {/* Sidebar */}
       <aside className="w-64 p-4 flex flex-col space-y-4">
         <div className="text-gray-900 text-xl font-semibold mb-6">Logo</div>
